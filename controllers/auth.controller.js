1a351642d2893bbb8b314f6c2215ffbdd89dcad5
romuald <sodouessodina@gmail.com>
2025-06-26 20:28:59 +0000

    #86 reset password controller  and route added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,14 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const db = require('../models');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const db = require("../models");
 const User = db.User;
-require('dotenv').config();
+require("dotenv").config();
 
 // Fonction pour générer un token JWT
 const generateToken = (user) => {
-  return jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+  return jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 };
 
 // REGISTER
@@ -15,7 +17,8 @@ const register = async (req, res) => {
     const { name, email, password } = req.body;
 
     const existingUser = await User.findOne({ where: { email } });
-    if (existingUser) return res.status(400).json({ message: 'Email déjà utilisé.' });
+    if (existingUser)
+      return res.status(400).json({ message: "Email déjà utilisé." });
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -23,12 +26,16 @@ const register = async (req, res) => {
       name,
       email,
       password: hashedPassword,
-      balance: 0 // Valeur par défaut
-      });
+      balance: 0, // Valeur par défaut
+    });
 
-    return res.status(201).json({ message: 'Utilisateur créé avec succès', userId: user.id });
+    return res
+      .status(201)
+      .json({ message: "Utilisateur créé avec succès", userId: user.id });
   } catch (error) {
-    return res.status(500).json({ message: 'Erreur serveur', error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Erreur serveur", error: error.message });
   }
 };
 
@@ -38,25 +45,33 @@ const login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
-    if (!user) return res.status(400).json({ message: 'Email ou mot de passe incorrect.' });
+    if (!user)
+      return res
+        .status(400)
+        .json({ message: "Email ou mot de passe incorrect." });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: 'Email ou mot de passe incorrect.' });
+    if (!isMatch)
+      return res
+        .status(400)
+        .json({ message: "Email ou mot de passe incorrect." });
 
     const token = generateToken(user); // Utilise ta fonction définie
 
     return res.status(200).json({
-      message: 'Connexion réussie',
+      message: "Connexion réussie",
       token,
       user: {
         id: user.id,
         name: user.name,
         email: user.email,
-        balance: user.balance
-      }
+        balance: user.balance,
+      },
     });
   } catch (error) {
-    return res.status(500).json({ message: 'Erreur serveur', error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Erreur serveur", error: error.message });
   }
 };
 
@@ -66,14 +81,48 @@ const getUserProfile = async (req, res) => {
     const userId = req.user.id;
 
     const user = await User.findByPk(userId, {
-      attributes: ['id', 'name', 'email', 'balance']
+      attributes: ["id", "name", "email", "balance"],
     });
 
-    if (!user) return res.status(404).json({ message: 'Utilisateur non trouvé.' });
+    if (!user)
+      return res.status(404).json({ message: "Utilisateur non trouvé." });
 
     return res.status(200).json({ user });
   } catch (error) {
-    return res.status(500).json({ message: 'Erreur serveur', error: error.message });
+    return res
+      .status(500)
+      .json({ message: "Erreur serveur", error: error.message });
+  }
+};
+// REINITIALISATION DU MOT DE PASSE
+const passwordReset = async (req, res) => {
+  const { newPassword } = req.body;
+  const { token } = req.query;
+
+  const { email, expiration } = tokenDB.get(token);
+
+  if (Date.now() > expiration) {
+    return res.status(400).json({ error: "le lien  est expiré" });
+  }
+
+  const passwordHash = await bcrypt.hash(newPassword, 10);
+
+  try {
+    const update = await User.update(
+      { password: passwordHash },
+      { where: { email: email } }
+    );
+
+    update !== 0
+      ? res.status(200).json({ message: "le mot de passe a été reinitialisé" })
+      : res
+          .status(400)
+          .json({ message: "Aucun utilisateur trouvé avec cet email" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "erreur serveur lors de la reinintialisation du mot de passe",
+    });
   }
 };
 
@@ -83,5 +132,5 @@ module.exports = {
   login,
   getUserProfile,
   generateToken,
-  
+  passwordReset,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/auth.controller');
-const { protect } = require('../middleware/authMiddleware');
+const authController = require("../controllers/auth.controller");
+const { protect } = require("../middleware/authMiddleware");
 
 // Routes pour l'authentification
-router.get('/profile', protect, authController.getUserProfile);
+router.get("/profile", protect, authController.getUserProfile);
 
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.post("/reset-password", authController.passwordReset);
 module.exports = router;
