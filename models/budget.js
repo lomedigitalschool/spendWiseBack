61cc047a33561675223efb83057a905e536f2ffb
yannick Namboure <yanoboston@gmail.com>
2025-06-26 15:05:31 +0000

    #81 Modelisation de la BD

---
+++ models/budget.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Budget = sequelize.define('Budget', {
+    user_id: { type: DataTypes.INTEGER, allowNull: false },
+    month: { type: DataTypes.INTEGER, allowNull: false, validate: { min: 1, max: 12 } },
+    year: { type: DataTypes.INTEGER, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false },
+    amount: { type: DataTypes.DECIMAL(10, 2), allowNull: false, validate: { min: 0 } }
+  }, {
+    tableName: 'Budgets',
+    timestamps: true,
+    underscored: true,
+    indexes: [{
+      unique: true,
+      fields: ['user_id', 'month', 'year']
+    }]
+  });
+
+  Budget.associate = models => {
+    Budget.belongsTo(models.User, { foreignKey: 'user_id' });
+    Budget.hasMany(models.BudgetCategory, { foreignKey: 'budget_id' });
+  };
+
+  return Budget;
+};
---
+++ models/budgetcategory.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const BudgetCategory = sequelize.define('BudgetCategory', {
+    budget_id: { type: DataTypes.INTEGER, allowNull: false },
+    category_id: { type: DataTypes.INTEGER, allowNull: false },
+    allocated_amount: { type: DataTypes.DECIMAL(10, 2), allowNull: false, validate: { min: 0 } },
+    alert_threshold: { type: DataTypes.INTEGER, validate: { min: 0, max: 100 } }
+  }, {
+    tableName: 'Budget_Categories',
+    timestamps: true,
+    underscored: true,
+    indexes: [{
+      unique: true,
+      fields: ['budget_id', 'category_id']
+    }]
+  });
+
+  BudgetCategory.associate = models => {
+    BudgetCategory.belongsTo(models.Budget, { foreignKey: 'budget_id' });
+    BudgetCategory.belongsTo(models.Category, { foreignKey: 'category_id' });
+    BudgetCategory.hasMany(models.Transaction, { foreignKey: 'budget_categories_id' });
+  };
+
+  return BudgetCategory;
+};
