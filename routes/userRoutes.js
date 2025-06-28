e12e3e953d08b52471e7f4ff14a78ec0312975db
yannick Namboure <yanoboston@gmail.com>
2025-06-28 00:48:40 +0000

    #80-endpoint-of-initial-balance

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const { protect } = require('../middleware/authMiddleware');
+const userController = require('../controllers/userController');
 
 // Routes pour l'authentification
 router.get('/profile', protect, authController.getUserProfile);
@@ -9,5 +10,13 @@ router.get('/profile', protect, authController.getUserProfile);
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+// POST /api/users/balance → définir le solde initial
+router.post('/balance', auth, userController.setInitialBalance);
+
+// GET /api/users/balance → récupérer le solde
+router.get('/balance', auth, userController.getBalance);
+
+
 
 module.exports = router;
