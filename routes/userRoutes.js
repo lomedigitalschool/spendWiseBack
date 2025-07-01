5963e05ed998ed2adee8e3697d1530e9b1d0ba9f
romuald <sodouessodina@gmail.com>
2025-07-01 21:04:21 +0000

    conflict fixiing

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const { protect } = require('../middleware/authMiddleware');
+
 // Routes pour l'authentification
 router.get('/profile', protect, authController.getUserProfile);
+
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/reset-password', authController.passwordReset);
+
 module.exports = router;
