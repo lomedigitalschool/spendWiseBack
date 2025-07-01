1ecd313e39cc250ac57d607c462d9a23b80bef68
romuald <sodouessodina@gmail.com>
2025-07-01 19:44:46 +0000

    fix conflict of single quote

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const authController = require("../controllers/auth.controller");
-const { protect } = require("../middleware/authMiddleware");
+const authController = require('../controllers/auth.controller');
+const { protect } = require('../middleware/authMiddleware');
 
 // Routes pour l'authentification
-router.get("/profile", protect, authController.getUserProfile);
+router.get('/profile', protect, authController.getUserProfile);
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 router.post(
-  "/password-reset-request",
+  '/password-reset-request',
   authController.passwordResetRequestController
 );
 
